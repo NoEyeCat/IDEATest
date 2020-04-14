b8182cf7cd702320e0b5a8e4f48f993481b22146
zx_glb <2405672660@qq.com>
2020-04-14 12:51:58 +0800

    Test for IDEA git commit.

---
+++ IDEATest/src/com/zx/demo/TestGit.java
@@ -0,0 +1,11 @@
+package com.zx.demo;
+
+/**
+ * @author ZX
+ * @date 2020/4/14--12:50
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("测试git");
+    }
+}
